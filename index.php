05becd2404e3ac0c189bdd44beb8b29d4aa0acb9
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-05-06 10:21:36 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
  * Description:       Add Security amd Speed to you website. Get free protection amd Optimization 
 
- * Version:           1.1
+ * Version:           1.3
 
  * Requires at least: 5.2
 
@@ -15,9 +15,9 @@
 
  * Author:            Nishant Shah
 
- * Author URI:        https://studentforum.xyz
+ * Author URI:      https://www.facebook.com/nepalinishantshah
 
- * License:           GPL v2l3
+ * License:           GPL v2.3
 
 
 
@@ -26,8 +26,6 @@
 
 $content = '
 # Compress text, html, javascript, css, xml:
-
-
 AddOutputFilterByType DEFLATE text/plain
 AddOutputFilterByType DEFLATE text/html
 AddOutputFilterByType DEFLATE text/xml
@@ -39,16 +37,13 @@ AddOutputFilterByType DEFLATE application/javascript
 AddOutputFilterByType DEFLATE application/x-javascript
 
 # Remove server signature
-
 ServerSignature Off
 
 # Remove ETags
-
 Header unset ETag
 FileETag None
 
 # Enable browser caching
-
 <IfModule mod_expires.c>
   ExpiresActive On
   ExpiresByType image/jpg "access plus 1 year"
@@ -79,8 +74,6 @@ FileETag None
 
 
 # Minify HTML
-
-
 <IfModule mod_deflate.c>
   AddOutputFilterByType DEFLATE text/html text/plain text/xml application/xml
 </IfModule>
@@ -88,48 +81,106 @@ FileETag None
 
 
 # Prevent cross-site scripting (XSS) attacks
-
 Header set X-XSS-Protection "1; mode=block"
 
 # Prevent cross-site request forgery (CSRF) attacks
-
 Header set X-Content-Type-Options "nosniff"
 Header set X-Frame-Options "SAMEORIGIN"
 
 # Protect against HTTP request smuggling attacks
-
 Header set Transfer-Encoding chunked
 
 # Secure cookie handling
-
 Header edit Set-Cookie ^(.*)$ $1;HttpOnly;Secure
 
-# Enable mod_security for added security
-
-<IfModule mod_security2.c>
-  SecRuleEngine On
-  SecRequestBodyAccess On
-  SecResponseBodyAccess On
-  SecDefaultAction "deny,log,status:500"
-  SecDebugLog /var/log/httpd/modsec_debug.log
-  SecDebugLogLevel 3
+# Serve images in next-gen formats
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{HTTP_ACCEPT} image/webp
+    RewriteCond %{REQUEST_FILENAME} \.(jpe?g|png)$
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteRule (.+)\.(jpe?g|png)$ $1.webp [T=image/webp,E=accept:1]
+    Header append Vary Accept env=REDIRECT_accept
 </IfModule>
+
+# Reduce unused CSS
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{REQUEST_FILENAME} \.css$
+    RewriteCond %{QUERY_STRING} !^no-min$
+    RewriteRule (.+)\.css$ $1.min.css [L,QSA]
+</IfModule>
+
+# Eliminate render-blocking resources
+<IfModule mod_headers.c>
+    <FilesMatch "\.(js|css)$">
+        Header set Cache-Control "max-age=31536000, public"
+    </FilesMatch>
+    <FilesMatch "\.(html|php)$">
+        Header set Cache-Control "max-age=300, private, must-revalidate"
+    </FilesMatch>
+</IfModule>
+
+
+# Protecting wp-config.php
+<Files wp-config.php>
+Order Allow,Deny
+Deny from all
+</files>
+
+#Protect .htaccess file
+<Files ~ "^.*\.([Hh][Tt][Aa])">
+order allow, deny
+deny from all
+satisfy all
+</Files>
+
+# Preventing Access to .htaccess File
+<Files .htaccess>
+order allow,deny
+deny from all
+</Files>
+
+# Speedshield Finsihed
 
 ';
 
-function edit_htaccess_file() {
+function active_speedshield() {
     $htaccess_file = ABSPATH . '.htaccess';
-    file_put_contents( $htaccess_file, $content, FILE_APPEND | LOCK_EX );
+    file_put_contents($htaccess_file, $content, FILE_APPEND | LOCK_EX);
+
+    // Define a function to remove the generator tag
+    function remove_generator_tag() {
+        return '';
+    }
+
+    // Hook the function to the 'the_generator' filter
+    add_filter('the_generator', 'remove_generator_tag');
+
+    // Access the theme's functions.php file and remove the generator tag
+    add_action('after_setup_theme', function() {
+        $functions_file = get_stylesheet_directory() . '/functions.php';
+        if (file_exists($functions_file)) {
+            require_once $functions_file;
+
+            // Change Error Message On Login Page
+            function no_wordpress_errors() {
+                return 'SOMETHING WENT WRONG!';
+            }
+            add_filter('login_errors', 'no_wordpress_errors');
+        }
+    });
 }
 
-function deactivate_edit_htaccess_file() {
+
+function deactivate_speedshield() {
     $htaccess_file = ABSPATH . '.htaccess';
     $existing_content = file_get_contents( $htaccess_file );
     $new_content = str_replace($content, "", $existing_content);
     file_put_contents( $htaccess_file, $new_content, LOCK_EX );
 }
 
-register_activation_hook( __FILE__, 'edit_htaccess_file' );
-register_deactivation_hook( __FILE__, 'deactivate_edit_htaccess_file' );
+register_activation_hook( __FILE__, 'active_speedshield' );
+register_deactivation_hook( __FILE__, 'deactivate_speedshield' );
 
 ?>
